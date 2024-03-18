21ed7f6b1310f47a94eb86e6c1702921c3144d6f
Paul Klinger <klinger.paul@gmail.com>
2024-03-18 22:26:21 +0000

    minor progress

--- web/collection.js
+++ web/collection.js
@@ -8,22 +8,34 @@ window.onload = () => {
 
   
   populate_works = (data) => {
-    const thumbnails_elem = document.getElementById("thumbnails");
-    for (var [work_id, work] of Object.entries(data.works)) {
-      const thumb_elem = document.createElement("div");
-      thumb_elem.classList.add("thumbnail");
-      const img_elem = document.createElement("img");
-      img_elem.setAttribute("src", "./data/imgs/works/thumbnails/" + work.main_img);
-      img_elem.classList.add("thumbnail_img");
-      thumb_elem.append(img_elem);
-      thumb_elem.work = work;
-      thumb_elem.onclick = () => {
-        thumb_elem.classList.remove("thumbnail");
-        thumb_elem.classList.add("main_image");
-        img_elem.setAttribute("src", "./data/imgs/works/full_res/" + this.work.main_img);
+    const works_elem = document.getElementById("main_list");
+    const parser = new DOMParser();
+    for (const [work_id, work] of Object.entries(data.works)) {
+      const main_img_thumb_path = "./data/imgs/works/thumbnails/" + work.main_img;
+      const main_img_full_path = "./data/imgs/works/full_res/" + work.main_img
+      work_thumbnails_html = ""
+      for (img of work.imgs) {
+        thumbnail_path = "./data/imgs/works/thumbnails/" + img;
+        work_thumbnails_html += `
+          <div class="work_thumbnail"><img src="${thumbnail_path}" /></div>
+        `
+      }
+
+      const work_elem = parser.parseFromString(`
+        <div class="work_thumb" id="work_${work_id}">
+          <img src="${main_img_thumb_path}" class="work_img">
+          <div class="work_thumbnails_bar" style="display: none;">
+            ${work_thumbnails_html}
+          </div>
+        </div>
+      `, "text/html").body.childNodes[0]
+      work_elem.onclick = () => {
+        work_elem.classList.remove("work_thumb");
+        work_elem.classList.add("work_full");
+        work_elem.querySelector(".work_img").setAttribute("src", main_img_full_path);
+        work_elem.querySelector(".work_thumbnails_bar").style.display = "block";
       };
-      thumbnails_elem.append(thumb_elem);
-      work.thumbnail_elem = thumb_elem;
+      works_elem.append(work_elem);
     }
   };
   
