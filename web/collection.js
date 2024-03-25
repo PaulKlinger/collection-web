0d5b864e71aabdb5a142f26e4e6f26bcb164693e
Paul Klinger <klinger.paul@gmail.com>
2024-03-25 21:10:36 +0000

    renaming

--- web/collection.js
+++ web/collection.js
@@ -10,6 +10,11 @@ window.onload = () => {
     });
 };
 
+elem_from_string = (s) => {
+  const parser = new DOMParser();
+  return parser.parseFromString(s, "text/html").body.childNodes[0];
+};
+
 setup_buttons = () => {
   document.querySelector("#works_select").onclick = () => {
     populate_works();
@@ -21,7 +26,6 @@ setup_buttons = () => {
 };
 
 create_work_elem = (work, artist) => {
-  const parser = new DOMParser();
   const main_img_thumb_path = "./data/imgs/works/thumbnails/" + work.main_img;
   const main_img_full_path = "./data/imgs/works/full_res/" + work.main_img;
   const artist_img_path = "./data/imgs/artists/" + artist.img;
@@ -30,17 +34,17 @@ create_work_elem = (work, artist) => {
   for (const [i, img] of all_imgs.entries()) {
     thumbnail_path = "./data/imgs/works/thumbnails/" + img;
     work_thumbnails_html += `
-      <div id="work_${work.work_id}_thumb_bar_img_${i}" class="work_thumb_bar_entry">
+      <div id="work_${work.work_id}_thumb_bar_img_${i}" class="entry_thumb_bar_entry">
         <img src="${thumbnail_path}" />
       </div>
     `;
   }
 
-  const work_elem = parser.parseFromString(
+  const work_elem = elem_from_string(
     `
-    <div class="work_thumb" id="work_${work.work_id}">
+    <div class="entry_thumb" id="work_${work.work_id}">
       <div class="close hide only_full">×</div>
-      <div class="work_info hide only_full">
+      <div class="entry_info hide only_full">
         <div class="work_artist">
           <img src="${artist_img_path}"/>
         </div>
@@ -53,39 +57,38 @@ create_work_elem = (work, artist) => {
           <p class="work_other">acquired: ${work.acquired}<br />from: ${work["bought where"]}</p>
         </div>
       </div>
-      <img src="${main_img_thumb_path}" class="work_img">
-      <div class="work_thumbnails_bar hide only_full">
+      <img src="${main_img_thumb_path}" class="entry_main_img">
+      <div class="entry_thumbnails_bar hide only_full">
         ${work_thumbnails_html}
       </div>
     </div>
-  `,
-    "text/html"
-  ).body.childNodes[0];
+  `
+  );
 
   for (const [i, img] of all_imgs.entries()) {
     work_elem.querySelector(
       `#work_${work.work_id}_thumb_bar_img_${i}`
     ).onclick = () => {
       work_elem
-        .querySelector(".work_img")
+        .querySelector(".entry_main_img")
         .setAttribute("src", "./data/imgs/works/full_res/" + img);
     };
   }
 
   work_elem.querySelector(".close").onclick = () => {
-    work_elem.className = "work_thumb";
+    work_elem.classList.replace("entry_full", "entry_thumb");
     work_elem
       .querySelectorAll(".only_full")
       .forEach((n) => n.classList.add("hide"));
     work_elem
-      .querySelector(".work_img")
+      .querySelector(".entry_main_img")
       .setAttribute("src", main_img_thumb_path);
   };
-  work_elem.querySelector(".work_img").onclick = () => {
-    if (work_elem.className === "work_thumb") {
-      work_elem.className = "work_full";
+  work_elem.querySelector(".entry_main_img").onclick = () => {
+    if (work_elem.classList.contains("entry_thumb")) {
+      work_elem.classList.replace("entry_thumb", "entry_full");
       work_elem
-        .querySelector(".work_img")
+        .querySelector(".entry_main_img")
         .setAttribute("src", main_img_full_path);
       work_elem
         .querySelectorAll(".only_full")
@@ -104,35 +107,36 @@ populate_works = () => {
 };
 
 create_artist_elem = (artist, works) => {
-  const parser = new DOMParser();
   const img_path = "./data/imgs/artists/" + artist.img;
 
-  const artist_elem = parser.parseFromString(
-    `
-    <div class="work_thumb" id="artist_${artist.id}">
+  let artist_country_str;
+  if (artist.residence !== artist.origin) {
+    artist_country_str = `${artist.origin} | ${artist.residence}`;
+  } else {
+    artist_country_str = artist.origin;
+  }
+
+  const artist_elem = elem_from_string(`
+    <div class="entry_thumb artist" id="artist_${artist.id}">
       <div class="close hide only_full">×</div>
       <div class="hide only_full">
-          <p class="artist_name">${artist.name}</p>
+        <p class="artist_name">${artist.name}</p>
+        <p class="artist_country">${artist_country_str}</p>
       </div>
-      <img src="${img_path}" class="work_img" />
+      <img src="${img_path}" class="entry_main_img" />
     </div>
-  `,
-    "text/html"
-  ).body.childNodes[0];
+  `);
 
   close_button = artist_elem.querySelector(".close");
   close_button.onclick = () => {
-    artist_elem.className = "work_thumb";
+    artist_elem.classList.replace("entry_full", "entry_thumb");
     artist_elem
       .querySelectorAll(".only_full")
       .forEach((n) => n.classList.add("hide"));
-    artist_elem
-      .querySelector(".work_img")
-      .setAttribute("src", main_img_thumb_path);
   };
-  artist_elem.querySelector(".work_img").onclick = () => {
-    if (artist_elem.className === "work_thumb") {
-      artist_elem.className = "work_full";
+  artist_elem.querySelector(".entry_main_img").onclick = () => {
+    if (artist_elem.classList.contains("entry_thumb")) {
+      artist_elem.classList.replace("entry_thumb", "entry_full");
       artist_elem
         .querySelectorAll(".only_full")
         .forEach((n) => n.classList.remove("hide"));
