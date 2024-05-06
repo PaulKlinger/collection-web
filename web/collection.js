07587ec0bf0eadb0468ca9a11705d54840cc7993
Paul Klinger <klinger.paul@gmail.com>
2024-05-06 20:31:25 +0100

    style tweaks

--- web/collection.js
+++ web/collection.js
@@ -162,8 +162,8 @@ const create_work_elem = (work, artist) => {
         </div>
         <div class="work_details">
           <p class="work_head">
-            <span class="work_title">${work.title}</span>
-            <span class="work_year">(${work.year})</span>
+            <span class="work_title">${work.title},</span>
+            <span class="work_year">${work.year}</span>
           </p>
           <p class="work_artist_name">${artist.name}</p>
           <p class="work_other">
